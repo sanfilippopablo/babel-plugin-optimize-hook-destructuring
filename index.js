abf4c177761a7a60f240b3e91f738e37c6276c40
Jason Miller <developit@users.noreply.github.com>
2019-08-15 10:27:02 -0400

    Improve performance by processing starting from hook invocations

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 module.exports = babel => {
   const { types: t } = babel;
+  const isHook = /^use[A-Z]/;
 
   return {
     name: "optimize-hook-destructuring",
     visitor: {
-      VariableDeclarator(path) {
-        if (t.isArrayPattern(path.node.id) && t.isCallExpression(path.node.init) && path.node.init.callee.name.match(/^use[A-Z]/)) {
-          path.node.id = t.objectPattern(
-            path.node.id.elements.map((element, i) =>
+      CallExpression(path) {
+        if (isHook.test(path.node.callee.name) && t.isArrayPattern(path.parent.id)) {
+          path.parent.id = t.objectPattern(
+            path.parent.id.elements.map((element, i) =>
               t.objectProperty(t.numericLiteral(i), element)
             )
           );
